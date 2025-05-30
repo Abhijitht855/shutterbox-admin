83e6e42fbe4e3c8db39753345b11a38a22066fec
Abhijith <abhijitht855@gmail.com>
2025-05-30 16:37:00 +0530

    commit

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,3 +1,50 @@
+// import React from 'react'
+// import { NavLink } from 'react-router-dom'
+// import { assets } from '../assets/assets'
+
+// const Sidebar = () => {
+//     return (
+//         <div className='w-[18%] min-h-screen border-r-2'>
+//             <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
+
+//                 <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/category">
+//                     <img className='w-5 h-5' src={assets.add_icon} alt="" />
+//                     <p className='hidden md:block'>Category</p>
+//                 </NavLink>
+
+//                 <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/add">
+//                     <img className='w-5 h-5' src={assets.add_icon} alt="" />
+//                     <p className='hidden md:block'>Add Items</p>
+//                 </NavLink>
+
+//                 <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/checkout">
+//                     <img className='w-5 h-5' src={assets.order_icon} alt="" />
+//                     <p className='hidden md:block'>Checkout</p>
+//                 </NavLink>
+
+
+//                 <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/orders">
+//                     <img className='w-5 h-5' src={assets.order_icon} alt="" />
+//                     <p className='hidden md:block'>Orders</p>
+//                 </NavLink>
+
+//                 <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/user">
+//                     <img className='w-5 h-5' src={assets.order_icon} alt="" />
+//                     <p className='hidden md:block'>User List </p>
+//                 </NavLink>
+
+//                 <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/list">
+//                     <img className='w-5 h-5' src={assets.order_icon} alt="" />
+//                     <p className='hidden md:block'>List Items</p>
+//                 </NavLink>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Sidebar
+
+
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets'
